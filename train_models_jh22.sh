22dfd1e3c0d3e349ef96c1591ce6ff680c116a63
Jaehun Choe <jaehunchoe827@gmail.com>
2025-11-04 10:25:23 -0500

    more yaml

--- train_models_jh22.sh
+++ train_models_jh22.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "Training ver3.yaml"
-python3 -m engine.main --train --config ver3.yaml
+echo "Training ver3_less_aug_more_rot.yaml"
+python3 -m engine.main --train --config ver3_less_aug_more_rot.yaml
 sleep 5
-echo "Training ver4.yaml"
-python3 -m engine.main --train --config ver4.yaml
 
 echo "All models trained successfully!"
--- train_models_mag24.sh
+++ train_models_mag24.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-echo "Training ver3_longer_epoch.yaml"
-python3 -m engine.main --train --config ver3_longer_epoch.yaml 
+echo "Training ver3_less_aug_longer_epoch_larger_batch.yaml"
+python3 -m engine.main --train --config ver3_less_aug_longer_epoch_larger_batch.yaml 
 sleep 5
-echo "Training ver3_larger_batch.yaml"
-python3 -m engine.main --train --config ver3_larger_batch.yaml 
-sleep 5
-echo "Training ver3_less_aug.yaml"
-python3 -m engine.main --train --config ver3_less_aug.yaml 
-sleep 5
-echo "Training ver3_no_edge.yaml"
-python3 -m engine.main --train --config ver3_no_edge.yaml 
-sleep 5
-echo "Training ver3_mae.yaml"
-python3 -m engine.main --train --config ver3_mae.yaml 
+echo "Training ver3_less_aug_longer_epoch_larger_batch_unfreeze_3.yaml"
+python3 -m engine.main --train --config ver3_less_aug_longer_epoch_larger_batch_unfreeze_3.yaml 
 
 echo "All models trained successfully!"
