6c68b859d3b3e325ad70fd9abc3deeea58811b44
Jaehun Choe <jaehunchoe827@gmail.com>
2025-11-04 16:02:21 -0500

    more configs

--- train_models_jh22.sh
+++ train_models_jh22.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Training ver3_less_aug_more_rot.yaml"
-python3 -m engine.main --train --config ver3_less_aug_more_rot.yaml
+echo "Training ver3_less_aug_longer_epoch_2.yaml"
+python3 -m engine.main --train --config ver3_less_aug_longer_epoch_2.yaml
 sleep 5
 
 echo "All models trained successfully!"
--- train_models_mag24.sh
+++ train_models_mag24.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo "Training ver3_less_aug_longer_epoch_larger_batch.yaml"
-python3 -m engine.main --train --config ver3_less_aug_longer_epoch_larger_batch.yaml 
+echo "Training ver3_less_aug_longer_epoch.yaml"
+python3 -m engine.main --train --config ver3_less_aug_longer_epoch.yaml 
 sleep 5
-echo "Training ver3_less_aug_longer_epoch_larger_batch_unfreeze_3.yaml"
-python3 -m engine.main --train --config ver3_less_aug_longer_epoch_larger_batch_unfreeze_3.yaml 
-
 echo "All models trained successfully!"
